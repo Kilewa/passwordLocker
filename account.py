638b45b118b0feb555e581534ebce62423370870
kilewa <kilewageorge230@gmail.com>
2020-09-06 21:47:22 +0300

    create a new user with a username and password

---
+++ account.py
@@ -0,0 +1,9 @@
+from users import User, Details
+
+
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
+    return new_user
