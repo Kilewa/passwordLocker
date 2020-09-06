daace9419513ca8d4d97d045215aafe57117487d
kilewa <kilewageorge230@gmail.com>
2020-09-06 21:22:26 +0300

    method that defines user Details to be stored

--- users.py
+++ users.py
@@ -38,5 +38,17 @@ class User:
         def verify_user(cls,username, password):
             """
             method to verify whether the user is in our user_list or not
-            """    
+            """   
+            a_user = ""
+            for user in User.user_list:
+                if(user.username == username and user.password == password):
+                        a_user == user.username
+            return a_user
+        def __init__(self,account,userName, password):
+            """
+            method that defines user Details to be stored
+                """
+            self.account = account
+            self.userName = userName
+            self.password = password    
     
