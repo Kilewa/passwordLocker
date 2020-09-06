543ac8d657da1823c2d7c5ab5676a58fdf080e73
kilewa <kilewageorge230@gmail.com>
2020-09-06 21:16:05 +0300

    created user class that geneates new user instances

--- users.py
+++ users.py
@@ -1,2 +1,17 @@
-def funcname(parameter_list):
-    pass
+import random
+import string
+import pyperclip
+
+class User:
+    """
+    Create User class that generates new instances of a user.
+
+    """
+    user_list = []
+
+    def __init__(self, username, password):
+        """
+        method that defines the properties of a user.
+        """
+        self.username = username
+        self.password = password
