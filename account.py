b3fe7523b21758dda19937cb6fbb045f54569388
kilewa <kilewageorge230@gmail.com>
2020-09-06 21:48:14 +0300

    save a new user

--- account.py
+++ account.py
@@ -7,3 +7,9 @@ def create_new_user(username,password):
     '''
     new_user = User(username,password)
     return new_user
+
+def save_user(user):
+    '''
+    Function to save a new user
+    '''
+    user.save_user()    
