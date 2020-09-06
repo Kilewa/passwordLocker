76b36383fbcfbe6d778b03c7e1a142585e8dbd34
kilewa <kilewageorge230@gmail.com>
2020-09-06 21:54:03 +0300

    unction to delete a details from details list

--- account.py
+++ account.py
@@ -17,8 +17,35 @@ def display_user():
     """
     Function to display existing user
     """
-    return User.display_user()
+    return User.display_user()      
 def login_user(username,password):
     """
     function that checks whether a user exist and then login the user in.
-    """        
+    """
+    check_user = Details.verify_user(username,password)
+    return check_user
+
+def create_new_detail(account,userName,password):
+    """
+    Function that creates new details for a given user account
+    """
+    new_detail = Details(account,userName,password)
+    return new_detail
+
+def save_detail(details):
+    """
+    Function to save details to the details list
+    """
+    details. save_details()
+def display_accounts_details():
+    """
+    Function that returns all the saved details.
+    """
+    return Details.display_details()
+
+def delete_detail(details):
+    """
+    Function to delete a details from details list
+
+    """
+    details.delete_details()    
